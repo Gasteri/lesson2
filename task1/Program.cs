fba90823653f300f74fda08edfdb39bdb40f304f
Gasteri <tipzov@yandex.ru>
2022-12-01 09:05:22 +0300

    random numbers

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+int number = random.Next(10,100);
+
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+
+
+
+if (firstNumber > lastNumber)
+{
+   Console.WriteLine(firstNumber); 
+   Console.WriteLine(number);
+} 
+else
+{
+    Console.WriteLine(lastNumber);
+    Console.WriteLine(number);
+}   
