e0ba068de7f39a6bc3011a4ff8299ab30ff87855
Gasteri <tipzov@yandex.ru>
2022-12-01 11:29:33 +0300

    multiplicity of numbers

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+if (a % 7 == 0 & a % 23 == 0)
+{
+    Console.Write(" -> кратно ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write(" -> не кратно ");
+    Console.Write(a);
+}
