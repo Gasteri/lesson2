563f30feb83dc7a17fdc1a4540448dff27ad4546
Gasteri <tipzov@yandex.ru>
2022-12-01 13:08:43 +0300

    the square of another number

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+int c = a * a;
+int g = b * b;
+
+if(c == b | g == a)
+{   
+    Console.WriteLine(" -> , Да");
+}
+else
+{
+    Console.WriteLine(" -> , нет");    
+}
+
+
